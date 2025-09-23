69708c6b2d8d7223c61c110f9abc4467190f4e4e
Fabrizio Italia <fabrizioitalia@MacBook-Pro-di-Fabrizio-2.local>
2025-09-23 12:57:51 +0200

    Other trials with extractor module

--- src/extractor_test.py
+++ src/extractor_test.py
@@ -1,6 +1,7 @@
 import requests
 import os 
 import process_mining
+import json
 
 #pmm = process_mining.ProcessMiningModule(os.path.join(os.path.dirname(__file__), '..', '10-Minute Sample.csv'))
 xes_path = os.path.join(os.path.dirname(__file__), 'DTLogExtSim', 'log_testing', 'log_test1.xes')
@@ -13,4 +14,18 @@ data = {
 }
 
 response = requests.post(url, files=files, data=data)
-print(response.json())
+parsed_response = response.json()
+
+output_dir = parsed_response["result"]["output_directory"]
+local_output_dir = os.path.join(os.path.dirname(__file__), "extractor_outputs", os.path.basename(output_dir.strip("/")), "output_data", "output_file")
+print("Local directory:", local_output_dir)
+files = os.listdir(local_output_dir)
+for file in files:
+    if "parameters" in file and "json" in file:
+        params_file = file
+
+factory_model_path = os.path.join(local_output_dir, params_file)
+
+with open(factory_model_path, "r") as f:
+    factory_model = json.load(f)
+print(factory_model)
